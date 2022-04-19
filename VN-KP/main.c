ae43432966bc2551528bfa918276ebf4e05a1396
Kush Prajapati <10055925+Kush-Prajapati@users.noreply.github.com>
2022-04-18 23:00:31 -0400

    Testing

--- VN-KP/main.c
+++ VN-KP/main.c
@@ -1,14 +1,26 @@
-//
-//  main.c
-//  VN-KP
-//
-//  Created by Kush Prajapati on 2022-04-18.
-//
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+    printf("00001a\n");
+    
+    float primary = 0.0;
+    float secondary = 0.0;
+    float out = 0.0;
+    
+    int i;
+    for (i = 0; i < 10; i++) {
+        
+        if (out == 0){
+            out = ++primary * ++secondary;
+        } else {
+            out *= 3.14;
+        }
+       
+        
+    }
+    printf("%f\n", out);
+    
     return 0;
 }

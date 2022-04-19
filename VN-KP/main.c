a4de718c2805cc191f23aa0edea256bc8a2a0112
Kush Prajapati <10055925+Kush-Prajapati@users.noreply.github.com>
2022-04-19 01:34:59 -0400

    Significantly improve and simplify the i/o structure

--- VN-KP/main.c
+++ VN-KP/main.c
@@ -4,45 +4,40 @@
 
 typedef struct {
     
-    float i1;
-    float i2;
-    float i3;
-    float i4;
+    float alpha;
+    float beta;
+    float gamma;
+    float delta;
     
-} input;
-
-typedef struct {
-    
-    float o1;
-    float o2;
-    float o3;
-    float o4;
-    
-} output;
-
+} io;
 
 int main(int argc, const char * argv[]) {
 
-    printf("00002a\n");
+    printf("00003a\n");
     
     bool result = false;
     
-    input input1 = {};
-   // output output1 = {};
+    io input = {};
     
-    if (input1.i1 > 1 && input1.i2 > 1) {
+    if (input.alpha > 1 && input.beta > 1) {
         
         result = true;
         
-        output output1 = {1, 1, 0.5, 0.5};
+        io output = {1, 1, 0.5, 0.5};
         
-    } else if (input1.i2 > 1 && input1.i4 > 1) {
+    } else if (input.gamma > 1 && input.delta > 1) {
         
-        output output1 = {0.5, 0.5, 1, 1};
+        io output = {0.5, 0.5, 1, 1};
         
     }
     
     printf("%d\n", result);
     
     return 0;
+}
+
+io neu1(io input) {
+    
+    
+    return input;
 }

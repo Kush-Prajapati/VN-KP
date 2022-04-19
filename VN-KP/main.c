d1bcefaca5fa55931e71ec39a89ac0d8f4460967
Kush Prajapati <kushprajapati@hotmail.com>
2022-04-18 22:13:08 -0400

    Initial Commit

---
+++ VN-KP/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  VN-KP
+//
+//  Created by Kush Prajapati on 2022-04-18.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

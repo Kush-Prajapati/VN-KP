6f5f5ab237ddf6cbc9ec1e049726047d9b966bef
Kush Prajapati <10055925+Kush-Prajapati@users.noreply.github.com>
2022-04-20 15:29:15 -0400

    use boolean instead

--- VN-KP/main.c
+++ VN-KP/main.c
@@ -4,40 +4,39 @@
 
 typedef struct {
     
-    float alpha;
-    float beta;
-    float gamma;
-    float delta;
+    bool alpha;
+    bool beta;
+    bool gamma;
+//    float delta;
     
 } io;
 
 int main(int argc, const char * argv[]) {
 
-    printf("00003a\n");
+    printf("00004a\n");
     
     bool result = false;
     
-    io input = {};
-    
-    if (input.alpha > 1 && input.beta > 1) {
-        
-        result = true;
-        
-        io output = {1, 1, 0.5, 0.5};
-        
-    } else if (input.gamma > 1 && input.delta > 1) {
-        
-        io output = {0.5, 0.5, 1, 1};
-        
-    }
+ //   io input = {};
     
     printf("%d\n", result);
     
     return 0;
 }
 
-io neu1(io input) {
+io neuone(io input) {
     
+    io out = {};
     
-    return input;
+    if (input.alpha && input.beta && input.gamma) {
+        
+        out = (io) {false, true, false};
+        
+    } else {
+        
+        out = (io) {true, false, true};
+        
+    }
+    
+    return out;
 }
